ff45e0c4e8aca453e70d8a95e408bd3e2820b198
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-11-09 23:45:46 +0800

    chore: release

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 <p align="center">
-<img height="200" src="./assets/kv.png" alt="to unocss">
+<img height="200" src="./assets/kv.png" alt="auto click">
 </p>
 <p align="center"> English | <a href="./README_zh.md">简体中文</a></p>
 
+![demo](assets/demo.gif)
 
 ## :coffee:
 
--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,73 @@
 import { addEventListener, getLineText, setSelection } from '@vscode-use/utils'
-import { ExtensionContext } from 'vscode'
+import type { ExtensionContext } from 'vscode'
+import { window } from 'vscode'
+
 export function activate(context: ExtensionContext) {
-  let pre: string | null = null
-  let count = 0
-  context.subscriptions.push(addEventListener('selection-change', e => {
-    const selection = e.selections[0]
-    if (!selection) {
-      pre = null
+  let timer: any = null
+  let isChanging = false
+  const STOP_REG = /[\s"\>\<\/{},':;\(\)@=]/
+  context.subscriptions.push(addEventListener('selection-change', (e) => {
+    if (timer)
+      clearTimeout(timer)
+    if (isChanging)
+      return
+    const selections = e.selections
+    if (selections.length !== 1)
+      return
+    const selection = selections[0]
+    if (selection.start.line === selection.end.line && selection.start.character === selection.end.character) {
+      // 单击，如果单机超过500ms，则自动选中多个内容
+      let start = selection.start.character
+      const line: number = selection.start.line
+      const lineText = getLineText(selection.start.line)
+      while (!STOP_REG.test(lineText[start - 1]) && start > 0)
+        start--
+      let end = selection.end.character
+      while (!STOP_REG.test(lineText[end]) && end < lineText.length)
+        end++
+      if ((start === selection.start.character) && (end === selection.end.character))
+        return
+
+      const newStart: [number, number] = [line, +start]
+      const newEnd: [number, number] = [line, +end]
+      timer = setTimeout(() => {
+        const editor = window.activeTextEditor?.document
+        if (!editor)
+          return
+        setSelection(newStart, newEnd)
+      }, 500)
       return
     }
-    const active = `${selection.active.line}-${selection.active.character}`
-    console.log({ pre, active })
-    if (!pre) {
-      pre = active
-      count = 1
-    } else if (isInSamePos(pre, active)) {
-      count++
-      if (count === 2) {
-        // 双击同一位置
-      } else if (count === 3) {
-        // 三击同一位置
-        if (selection.start.line === selection.end.line && selection.start.character === selection.end.character) {
-          count = 1
-          debugger
-          return
-        }
-        let start = selection.start.character
-        const lineText = getLineText(selection.start.line)
-        while (!/\s"\>\<\//.test(lineText[start]) && start > 0) {
-          start--
-        }
-        let end = selection.end.character
-        while (!/\s"\>\<\//.test(lineText[end]) && end < lineText.length) {
-          end++
-        }
-        setSelection([selection.start.line, start], [selection.end.line, end])
-        count = 0
-        debugger
-      }
-    } else {
-      pre = active
-      count = 1
-    }
+    let start = selection.start.character
+    const line: number = selection.start.line
+    const lineText = getLineText(selection.start.line)
+    while (!STOP_REG.test(lineText[start - 1]) && start > 0)
+      start--
+    let end = selection.end.character
+    while (!STOP_REG.test(lineText[end]) && end < lineText.length)
+      end++
+    if ((start === selection.start.character) && (end === selection.end.character))
+      return
 
+    const newStart: [number, number] = [line, +start]
+    const newEnd: [number, number] = [line, +end]
+
+    // 如果间隔500ms没有新的改变则选中大面积
+    timer = setTimeout(() => {
+      const editor = window.activeTextEditor?.document
+      if (!editor)
+        return
+      setSelection(newStart, newEnd)
+    }, 500)
+  }))
+  context.subscriptions.push(addEventListener('text-change', () => {
+    isChanging = true
+    if (timer)
+      clearTimeout(timer)
+    setTimeout(() => isChanging = false, 50)
   }))
 }
 
 export function deactivate() {
 
-}
-
-function isInSamePos(pre: string, cur: string) {
-  const [preLine, preColumn] = pre.split('-')
-  const [curLine, curColumn] = cur.split('-')
-  if (preLine !== curLine)
-    return false
-  const lineText = getLineText(+preLine)
-  const min = Math.min(+preColumn, +curColumn)
-  const max = Math.max(+preColumn, +curColumn)
-  for (let i = min; i < max; i++) {
-    const mid = lineText[i]
-    if (!/[\w_]/.test(mid))
-      return false
-  }
-  return true
 }
